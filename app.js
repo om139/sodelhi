ddfacfca15adee68dea1e6372f3277060af28b58
om139 <omprakash1993@outlook.com>
2017-01-10 19:26:35 +0530

    adding

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose= require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+mongoose.connect("mongodb://188.166.187.120:27017");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
